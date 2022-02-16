1855c59886e3c0d4778b810260278377aca2fef3
Sergey Bulychev <bulycheff@ya.ru>
2022-02-16 21:05:23 +0500

    release version 2: added models

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
 app.use(serveStatic(__dirname))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
 var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
